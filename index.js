d61d6c974104e1aa2657e1ea73d08c8a48071097
Anonymous <>
2024-11-21 10:03:33 +0100

    4_1.1

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ const app = express();
 
 // define endpoint for exercise 1 here
 app.get('/math/circle/:r', (req, res) => {
-//TODO1  
-  res.json(result);
+  // 4_1.1
+  const radius = req.params.r;
+  if(!radius) {
+    const errorMessage = "Missing Required GET parameters: r";
+    return res.status(400).send(errorMessage);
+  }
+  var obj = {
+    area: Math.pow(radius, 10) * Math.PI,
+    circumference: Math.PI * radius * 2.0,
+  }
+
+  res.json(obj);
 });
 
 //TODO2
